583e4534f3040782f47e677e81639a222197ea9d
vstung <vstungz@gmail.com>
2023-05-16 08:19:46 +0700

    Change try catch to decorator

--- app.py
+++ app.py
@@ -11,9 +11,18 @@ app = Flask(__name__)
 api = Api(app)
 
 DATA_DIR = '_files'
+DEBUG = os.environ.get("DEBUG", False)
 class ITEMTYPE:
     DIRECTORY = "directory"
     FILE = "file"
+
+def os_exception_handle(f):
+    def _inner_func(*args, **kwargs):
+        try:
+            return f(*args, **kwargs)
+        except OSError as e:
+            return {'error_message': f'{e.filename}: {e.strerror}', }, 400
+    return _inner_func
 
 
 class Directory(Resource):
@@ -29,6 +38,7 @@ class Directory(Resource):
                     dirs.append(item.name)
         return {'dirs': dirs, 'files': files}
 
+    @os_exception_handle
     def get(self, path = ''):
         full_path = os.path.sep.join([DATA_DIR, path])
         response = {
@@ -41,10 +51,6 @@ class Directory(Resource):
 
 class File(Resource):
 
-    # def _read_file(self, path):
-    #     with open(path, 'rb') as f:
-    #         return b64encode(f.read()).decode('utf-8')
-
     def get(self, path):
         full_path = os.path.sep.join([DATA_DIR, path])
         response = {
@@ -53,6 +59,7 @@ class File(Resource):
         }
         return response
     
+    @os_exception_handle
     def delete(self, path):
         full_path = os.path.sep.join([DATA_DIR, path])
         try:
@@ -61,6 +68,7 @@ class File(Resource):
         except Exception as e:
             return e
         
+    @os_exception_handle
     def patch(self, path):
         full_path = os.path.sep.join([DATA_DIR, path])
 
@@ -75,65 +83,63 @@ class File(Resource):
         except Exception as e:
             return e
 
+    @os_exception_handle
     def post(self, path):
         full_path = os.path.sep.join([DATA_DIR, path])
         req = request.json
 
-        try:
-            # create new file by posting content
-            if request.args['action'] == None or request.args['action'] == 'upload':
-                if req['content'] == None:
-                    return None, 400
-
-                with open(full_path, 'w') as f:
-                    f.write(req['content'])
-                return File().get(path)
-                
-            # create new file by scraping from direct URL
-            elif request.args['action'] == 'scrape':
-                if req['url'] == None:
-                    return None, 400
-
-                response = urlopen(req['url'])
-                if response.status not in [200]:
-                    return {'error_message': f'{req["url"]}: {response.status} - {response.reason}'}
-
-                CHUNK = 16 * 1024
-                with open(full_path, 'wb') as f:
-                    while True:
-                        chunk = response.read(CHUNK)
-                        if not chunk:
-                            break
-                        f.write(chunk)
-                return File().get(path)
-                
-            # create new file by zip multiple files
-            elif request.args['action'] == 'zip':
-                if req['files'] == None or not isinstance(req['files'], list):
-                    return None, 400
-                
-                with ZipFile(full_path, 'x') as zipObj:
-                    for _file_path in req['files']:
-                        zipObj.write(os.path.sep.join([DATA_DIR, _file_path]))
-                return File().get(path) 
-
-            # create new file by concat multiple files
-            elif request.args['action'] == 'concat':
-                if req['files'] == None or not isinstance(req['files'], list):
-                    return None, 400
-                
-                with open(full_path, 'wb') as target_f:
-                    for _file_path in req['files']:
-                        with open(os.path.sep.join([DATA_DIR, _file_path]), 'rb') as src_f:
-                            target_f.write(src_f.read())
-                resp = File().get(path) 
-                return resp
-                
-            else:
+        # create new file by posting content
+        if request.args['action'] == None or request.args['action'] == 'upload':
+            if req['content'] == None:
                 return None, 400
+
+            with open(full_path, 'w') as f:
+                f.write(req['content'])
+            return File().get(path)
+            
+        # create new file by scraping from direct URL
+        elif request.args['action'] == 'scrape':
+            if req['url'] == None:
+                return None, 400
+
+            response = urlopen(req['url'])
+            if response.status not in [200]:
+                return {'error_message': f'{req["url"]}: {response.status} - {response.reason}'}
+
+            CHUNK = 16 * 1024
+            with open(full_path, 'wb') as f:
+                while True:
+                    chunk = response.read(CHUNK)
+                    if not chunk:
+                        break
+                    f.write(chunk)
+            return File().get(path)
+            
+        # create new file by zip multiple files
+        elif request.args['action'] == 'zip':
+            if req['files'] == None or not isinstance(req['files'], list):
+                return None, 400
+            
+            with ZipFile(full_path, 'x') as zipObj:
+                for _file_path in req['files']:
+                    zipObj.write(os.path.sep.join([DATA_DIR, _file_path]))
+            return File().get(path) 
+
+        # create new file by concat multiple files
+        elif request.args['action'] == 'concat':
+            if req['files'] == None or not isinstance(req['files'], list):
+                return None, 400
+            
+            with open(full_path, 'wb') as target_f:
+                for _file_path in req['files']:
+                    with open(os.path.sep.join([DATA_DIR, _file_path]), 'rb') as src_f:
+                        target_f.write(src_f.read())
+            resp = File().get(path) 
+            return resp
+            
+        else:
+            return None, 400
         
-        except OSError as e:
-                return {'error_message': f'{e.filename}: {e.strerror}', }, 400
 
 
 
@@ -146,5 +152,5 @@ api.add_resource(File, '/file/<path:path>')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', threaded=True, port=5000, debug=True)
+    app.run(host='0.0.0.0', threaded=True, port=5000, debug=DEBUG)
 
