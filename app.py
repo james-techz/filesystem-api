50d21adfd491e7215acedd31d095ef9a912045e4
vstung <vstungz@gmail.com>
2023-05-16 08:39:49 +0700

    Fix file path in OSErro excepttion message

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def os_exception_handle(f):
         try:
             return f(*args, **kwargs)
         except OSError as e:
-            trimmed_filename = e.filename.split(os.path.sep)[1:]
+            trimmed_filename = os.path.sep.join(e.filename.split(os.path.sep)[1:])
             return {'error_message': f'{trimmed_filename}: {e.strerror}', }, 400
     return _inner_func
 
