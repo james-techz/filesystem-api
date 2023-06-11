a3f7fefac2c00d6705b79ea983b58df56443f35b
vstung <vstungz@gmail.com>
2023-06-11 18:43:40 +0700

    add condition to debug the built image

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
-#/bin/ash
+#!/bin/bash
 
 /usr/sbin/nginx
-/usr/local/bin/gunicorn -b 0.0.0.0:8000 -w 4 --threads 128 --access-logfile - --error-logfile - app:app 
+
+if [[ -z "${DEBUG}" ]]; then
+    /usr/local/bin/gunicorn -b 0.0.0.0:8000 -w 4 --threads 128 --access-logfile - --error-logfile - app:app
+else
+    python app.py
+fi
